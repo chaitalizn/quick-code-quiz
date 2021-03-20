ea8328abb40dda81c2b8f34e2144cce2fd489720
chaitalizn <phchaitali3@gmail.com>
2021-03-20 14:44:51 -0400

    create question and answer for quiz

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+var gameIntro = document.querySelector(".main-flex");
+
+
+
+
+/*
+1 Inside which HTML element do we put the JavaScript?
+A <script>
+<js>
+<scripting>
+<javascript>
+
+2 What is the correct JavaScript syntax to change the content of the HTML element below?
+
+<p id="demo">This is a demonstration.</p>
+#demo.innerHTML = "Hello World!"
+document.getElement("p").innerHTML = "Hello World!"
+document.getElementByName("p").innerHTML = "Hello World!"
+A document.getElementById("demo").innerHTML = "Hello World!"
+
+3 Where is the correct place to insert a JavaScript?
+The <body> section
+A Both the <head> section and the <body> section are correct
+The <head> section
+
+4 How do you create a function in JavaScript?
+function:myFunction()
+A function myFunction()
+function = myFunction()
+
+5 How to write an IF statement in JavaScript?
+if i == 5 then
+if i = 5
+A if (i == 5)
+if i = 5 then
+
+6 How to write an IF statement for executing some code if "i" is NOT equal to 5?
+if i <> 5
+if i =! 5 then
+if (i <> 5)
+A if (i != 5)
+
+7 How does a WHILE loop start?
+while i = 1 to 10
+A while (i <= 10)
+while (i <= 10; i++)
+
+8 How does a FOR loop start?
+for (i <= 5; i++)
+for i = 1 to 5
+for (i = 0; i <=5)
+A for (i=0; i <= 5; i ++)
+
+9 What is the correct way to write a JavaScript array?
+A var color = ["red", "green", "blue"]
+var color = "red", "green", "blue"
+var color = 1 = ("red"), 2 = ("green"), 3 = ("blue")
+var color = (1:"red", 2:"green", 3:"blue")
+
+10 How do you round the number 7.25, to the nearest integer?
+A Math.round(7.25)
+rnd(7.25)
+Math.rnd(7.25)
+round(7.25)
+
+*/
