1fc09bd2e3064005409be996e369af95bda0f6d2
chaitalizn <phchaitali3@gmail.com>
2021-03-20 16:57:32 -0400

    displayQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,66 @@
-var gameIntro = document.querySelector(".main-flex");
 
+var questions = [
+    {question: "Inside which HTML element do we put the JavaScript?",
+    option1: "<script>",
+    option2: "<scripting>",
+    option3: "<javascript>",
+    answer: "<script>"
+},
+    {question: "What is the correct JavaScript syntax to change the content of this <p id='demo'>This is a demonstration.</p> HTML element?",
+    option1: "document.getElementName('p').innerHTML = 'Hello World!'",
+    option2: "document.getElement('p').innerHTML = 'Hello World!'",
+    option3: "document.getElementById('demo').innerHTML = 'Hello World!'",
+    answer: "document.getElementById('demo').innerHTML = 'Hello World!'"
+},
+    {question: "Where is the correct place to insert a JavaScript?",
+    option1: "The <body> section",
+    option2: "Both the <head> section and the <body> section are correct",
+    option3: "The <head> section",
+    answer: "Both the <head> section and the <body> section are correct"
+}
+]
+
+var buttonEl = document.querySelector("#start-btn");
+var quizIntor = document.querySelector("#quiz-intro");
+var mainEl = document.querySelector("#main");
+
+
+
+buttonEl.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    quizIntor.remove();
+    displayQuestion();
+}
+
+function displayQuestion() {
+    for (i = 0; i < questions.length; i++) {
+    var question = document.createElement("h2");
+    question.textContent = questions[i].question;
+    mainEl.appendChild(question);
+    question.className = "main-flex";
+
+    var optionButton1 = document.createElement("button");
+    optionButton1.textContent = questions[i].option1;
+    question.appendChild(optionButton1);
+    optionButton1.className= "option-btn";
+
+    var optionButton2 = document.createElement("button");
+    optionButton2.textContent = questions[i].option2;
+    question.appendChild(optionButton2);
+    optionButton2.className= "option-btn";
+
+    var optionButton3 = document.createElement("button");
+    optionButton3.textContent = questions[i].option3;
+    question.appendChild(optionButton3);
+    optionButton3.className= "option-btn";
+
+}
+}
 
 
 
 /*
-1 Inside which HTML element do we put the JavaScript?
-A <script>
-<js>
-<scripting>
-<javascript>
-
-2 What is the correct JavaScript syntax to change the content of the HTML element below?
-
-<p id="demo">This is a demonstration.</p>
-#demo.innerHTML = "Hello World!"
-document.getElement("p").innerHTML = "Hello World!"
-document.getElementByName("p").innerHTML = "Hello World!"
-A document.getElementById("demo").innerHTML = "Hello World!"
-
-3 Where is the correct place to insert a JavaScript?
-The <body> section
-A Both the <head> section and the <body> section are correct
-The <head> section
-
 4 How do you create a function in JavaScript?
 function:myFunction()
 A function myFunction()
@@ -32,10 +70,9 @@ function = myFunction()
 if i == 5 then
 if i = 5
 A if (i == 5)
-if i = 5 then
 
 6 How to write an IF statement for executing some code if "i" is NOT equal to 5?
-if i <> 5
+
 if i =! 5 then
 if (i <> 5)
 A if (i != 5)
@@ -47,19 +84,16 @@ while (i <= 10; i++)
 
 8 How does a FOR loop start?
 for (i <= 5; i++)
-for i = 1 to 5
 for (i = 0; i <=5)
 A for (i=0; i <= 5; i ++)
 
 9 What is the correct way to write a JavaScript array?
 A var color = ["red", "green", "blue"]
-var color = "red", "green", "blue"
 var color = 1 = ("red"), 2 = ("green"), 3 = ("blue")
 var color = (1:"red", 2:"green", 3:"blue")
 
 10 How do you round the number 7.25, to the nearest integer?
 A Math.round(7.25)
-rnd(7.25)
 Math.rnd(7.25)
 round(7.25)
 
